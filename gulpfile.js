e7dcfe8ddd756b8b6e6235f76e79ac5049615e7f
Ryan Bush <96.rbush@gmail.com>
2017-08-05 15:48:24 +0100

    Added min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 
 gulp.task('sass', function(){
   return gulp.src('sass/framework.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('sassmin', function(){
+  return gulp.src('sass/framework.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename('framework.min.css'))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('default', function () {
+    gulp.start('sass');
+    gulp.start('sassmin');
 });
